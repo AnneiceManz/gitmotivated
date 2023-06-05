7cab803d30f1eab7ed780ded7e02f2dca17133a1
Mifrah Malik <mifrah.malik@gmail.com>
2023-06-05 13:10:56 -0400

    started to update the fetch

--- client/src/components/ListStudents.jsx
+++ client/src/components/ListStudents.jsx
@@ -11,8 +11,8 @@ const ListQuotes = () => {
     // //this is the state needed for the UpdateRequest
     // const [editingStudent, setEditingStudent] = useState(null)
 
-    const loadStudents = () => {
-        // A function to fetch the list of students that will be load anytime that list change
+    const loadQuotes = () => {
+        // A function to fetch the list of quotes that will be load anytime that list change
         fetch("http://localhost:8080/api/students")
             .then((response) => response.json())
             .then((students) => {
