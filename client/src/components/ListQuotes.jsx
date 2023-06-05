35debad79ad94df7745aeebb242b59f280efab43
Anneice Manzanares <mountains.stars@gmail.com>
2023-06-05 12:04:04 -0600

    Remove unused code for updating and deleting quotes
    The code changes remove the commented out functions that handle updating and deleting quotes. These functions are not used in the current implementation of the ListQuotes component.

--- client/src/components/ListQuotes.jsx
+++ client/src/components/ListQuotes.jsx
@@ -5,9 +5,6 @@ const ListQuotes = () => {
   // this is my original state with an array of quotes
   const [quotes, setQuotes] = useState([]);
 
-  // //this is the state needed for the UpdateRequest
-  // const [editingStudent, setEditingStudent] = useState(null)
-
   const loadQuotes = () => {
     // A function to fetch the list of quotes that will be load anytime that list change
     fetch("http://localhost:8080/api/quotes")
@@ -21,37 +18,6 @@ const ListQuotes = () => {
     loadQuotes();
   }, []);
 
-  // const onSaveStudent = (newStudent) => {
-  //     //console.log(newStudent, "From the parent - List of Students");
-  //     setStudents((students) => [...students, newStudent]);
-  // }
-
-  // //A function to control the update in the parent (student component)
-  // const updateStudent = (savedStudent) => {
-  //     // console.log("Line 29 savedStudent", savedStudent);
-  //     // This function should update the whole list of students -
-  //     loadStudents();
-  // }
-
-  // //A function to handle the Delete funtionality
-  // const onDelete = (student) => {
-  //     //console.log(student, "delete method")
-  //     return fetch(`http://localhost:8080/api/students/${student.id}`, {
-  //         method: "DELETE"
-  //     }).then((response) => {
-  //         //console.log(response);
-  //         if (response.ok) {
-  //             loadStudents();
-  //         }
-  //     })
-  // }
-
-  // //A function to handle the Update functionality
-  // const onUpdate = (toUpdateStudent) => {
-  //     //console.log(toUpdateStudent);
-  //     setEditingStudent(toUpdateStudent);
-
-  // }
 
   return (
     <div className="mybody">
