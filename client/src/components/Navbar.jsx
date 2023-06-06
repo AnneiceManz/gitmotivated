09a7d7c69b5be955dda03e327d122f5c0fe2092c
Mifrah Malik <mifrah.malik@gmail.com>
2023-06-06 14:21:28 -0400

    deleted more from navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -17,11 +17,7 @@ function MyNavBar(props) {
               alt="React Bootstrap logo"
             />
           </Navbar.Brand>
-          <Nav.Link>Your Link</Nav.Link>
-          <Navbar.Toggle />
-          <Navbar.Collapse className="justify-content-end">
-            <Navbar.Text></Navbar.Text>
-          </Navbar.Collapse>
+          
         </Container>
       </Navbar>
     </>
