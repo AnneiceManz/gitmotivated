28d8cb3b6e7e768688b45d5bba693dcd03dc2de5
Mifrah Malik <mifrah.malik@gmail.com>
2023-06-06 14:20:25 -0400

    deleted some lines of code

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,52 +1,31 @@
-import React, { useState } from 'react';
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import Logo from '../assets/1.png'
-
+import React, { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Logo from "../assets/1.png";
 
 function MyNavBar(props) {
-
-  const [quotes, setQuotes] = useState([])
-  
-  const fecthQuotes = async (keyword) => {
-    try {
-      const response= await fetch(`http://localhost:8080/api/keyword/${keyword}`)
-      const quotes = response.data[4]
-      setQuotes(quotes)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  const handleChange = (event) => {
-    const keyword = event.target.value
-    fecthQuotes(keyword)
-  }
-
   return (
     <>
-    <Navbar bg="light" variant="light" sticky="top" className='navbar'>
-      <Container>
-        <Navbar.Brand href="/">
-        <img
+      <Navbar bg="light" variant="light" sticky="top" className="navbar">
+        <Container>
+          <Navbar.Brand href="/">
+            <img
               src={Logo}
               height="180"
               className="d-lg-inline-block"
               alt="React Bootstrap logo"
             />
-        </Navbar.Brand>
-        <Nav.Link >Your Link</Nav.Link>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            
-          </Navbar.Text>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+          </Navbar.Brand>
+          <Nav.Link>Your Link</Nav.Link>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text></Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
   );
-};
+}
 
 export default MyNavBar;
